47605f99dc781c448dd899b8c2018d2848091334
AlexGeek <alexpopdj@gmail.com>
2020-07-11 16:09:27 -0500

    adding Janz React

---
+++ testing/janz/precache-manifest.4d08af366b8efda3b17f72141ddd9e6f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd88f3bbd0b174cc7269f50c5fab5e1",
+    "url": "/testing/janz/index.html"
+  },
+  {
+    "revision": "c38098fbb901263b7ffa",
+    "url": "/testing/janz/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e3ce3c866475b5fe011f",
+    "url": "/testing/janz/static/css/main.881d03e5.chunk.css"
+  },
+  {
+    "revision": "c38098fbb901263b7ffa",
+    "url": "/testing/janz/static/js/2.a622a474.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testing/janz/static/js/2.a622a474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ce3c866475b5fe011f",
+    "url": "/testing/janz/static/js/main.72078ed6.chunk.js"
+  },
+  {
+    "revision": "6e7657b077f78eeae61c",
+    "url": "/testing/janz/static/js/runtime-main.a55857de.js"
+  },
+  {
+    "revision": "70d0211393466d3213e4d495f1fd034b",
+    "url": "/testing/janz/static/media/Logo.70d02113.png"
+  },
+  {
+    "revision": "2c43aaae2c1492ba0e6a6a26c6d82b05",
+    "url": "/testing/janz/static/media/UserProfile (1).2c43aaae.png"
+  },
+  {
+    "revision": "158dd1f14ad9514cc4bf698c7bd8de80",
+    "url": "/testing/janz/static/media/UserProfile (2).158dd1f1.png"
+  },
+  {
+    "revision": "9f3fc3efc2f8be3192f9d39a41045959",
+    "url": "/testing/janz/static/media/UserProfile (5).9f3fc3ef.png"
+  },
+  {
+    "revision": "317f22330f7a0ef5b513786eeccd5646",
+    "url": "/testing/janz/static/media/background.317f2233.png"
+  },
+  {
+    "revision": "0845de241541f331c06955ea450a5407",
+    "url": "/testing/janz/static/media/janz.0845de24.png"
+  },
+  {
+    "revision": "f24a9c09a3fd4e457f6e5152ccd4013b",
+    "url": "/testing/janz/static/media/person2.f24a9c09.png"
+  },
+  {
+    "revision": "1121edaa5d34e9888e31c05d382a8ab0",
+    "url": "/testing/janz/static/media/scratch degradado.1121edaa.png"
+  },
+  {
+    "revision": "f213ad28a1e70dc0f3baa9d7f73325db",
+    "url": "/testing/janz/static/media/video.f213ad28.mp4"
+  }
+]);
